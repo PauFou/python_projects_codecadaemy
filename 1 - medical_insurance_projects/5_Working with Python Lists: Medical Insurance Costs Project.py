1a74d2d4ddb8f5f152d48d4f11f51858be3df59d
Paul Fournier <pfournier597@gmail.com>
2022-10-10 15:37:36 +0200

    excercise 4&5

---
+++ 1 - medical_insurance_projects/5_Working with Python Lists: Medical Insurance Costs Project.py
@@ -0,0 +1,28 @@
+#You are a doctor sorting through medical insurance cost data for some patients.
+
+#Using your knowledge of Python lists, you will store medical data and see what valuable insights you can gain from that data.
+
+names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily", "Nikita", "Paul"]
+
+insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
+
+# Add your code here
+
+names.append("Priscilla")
+insurance_costs.append(8320.0)
+medical_records = list(zip(insurance_costs, names))
+print(medical_records)
+num_medical_records = len(medical_records)
+print("There are"+ str(num_medical_records))
+first_medical_record = medical_records[0]
+print("Here is the first medical record: " + str(first_medical_record))
+medical_records.sort()
+print(medical_records)
+cheapest_three = medical_records[:3]
+print("Here are the three cheapest insurance costs in our medical records: " + str(cheapest_three))
+
+priciest_three = medical_records[3:]
+print("Here are the three most expensive insurance costs in our medical records: " + str(priciest_three))
+
+occurrences_paul = names.count("Paul")
+print("There are " + str(occurrences_paul) + " individuals with the name Paul in our medical records.")
